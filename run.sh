c57e17bc2bd16d142213a588a69918a750c4b7b6
xfocus3 <xfocus29@gmail.com>
2020-04-02 20:19:14 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,4 @@
-#!/bin/bash
-if [ $# -lt 4 ];then
-    echo "Usage: $0  <service URL> <partner ID> <partner admin secret> </path/to/file> [optional entryId to update]"
-    exit 1
-fi
-
+#!/bin/bash 
 SERVICE_URL=$1
 PARTNER_ID=$2
 ADMIN_SECRET=$3
@@ -18,4 +13,3 @@ javac -cp .:KalturaClient-3.3.1.jar:httpclient-4.2.3.jar:commons-httpclient-3.1.
 
 #run
 java -cp .:KalturaClient-3.3.1.jar:httpclient-4.2.3.jar:commons-httpclient-3.1.jar:log4j-1.2.15.jar:json-20090211.jar:commons-codec-1.6.jar:commons-logging-1.1.1.jar UploadTest $SERVICE_URL $PARTNER_ID $ADMIN_SECRET $PATH_TO_FILE $ENTRY_ID_TO_UPDATE 
-
